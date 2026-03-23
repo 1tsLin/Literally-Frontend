8f64d556b2ec83f2ab570c472fc79d7e63f93acb
Lin <julinejiang@gmail.com>
2026-03-23 10:38:00 +0100

    Footer : add footer

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,15 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [ReactiveFormsModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-  private route = inject(ActivatedRoute);
-
-  ngOnInit() {}
-}
+export class HeaderComponent {}
