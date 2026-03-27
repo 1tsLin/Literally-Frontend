9ee742c0aa300ca814ac9035e37e7172eaddbf9d
Lin <julinejiang@gmail.com>
2026-03-27 17:36:23 +0100

    Product configuration : [WIP]

--- src/app/core/catalog/catalog.component.ts
+++ src/app/core/catalog/catalog.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CatalogProductComponent } from './catalog-product/catalog-product.component';
 import { catalogProducts } from '../../shared/dummy-data';
 import { CatalogFiltersComponent } from './catalog-filters/catalog-filters.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
-  imports: [CatalogProductComponent, CatalogFiltersComponent],
+  imports: [CatalogProductComponent, CatalogFiltersComponent, RouterLink],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css',
 })
--- src/app/shared/components/product/product-configuration/product-configuration.component.ts
+++ src/app/shared/components/product/product-configuration/product-configuration.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { SectionHeaderComponent } from '../../section-header/section-header.component';
 
 @Component({
   selector: 'app-product-configuration',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, SectionHeaderComponent],
   templateUrl: './product-configuration.component.html',
   styleUrl: './product-configuration.component.css',
 })
@@ -12,11 +13,15 @@ export class ProductConfigurationComponent {
   private route = inject(ActivatedRoute);
   id?: string;
 
+  heroTitle: String = '(nouveau produit)';
+  isGeneralInfoOpen: boolean = false;
+
   ngOnInit() {
     this.id = this.route.snapshot.queryParamMap.get('id') ?? undefined;
 
     if (this.id) {
       // UPDATE
+      this.heroTitle = '(id n° ' + this.id + ')';
     } else {
       // CREATE
     }
