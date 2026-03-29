b6d951f1b5c1dbcb471e4199d28cc613da1d89e6
Lin <julinejiang@gmail.com>
2026-03-29 22:57:11 +0200

    [WIP]

--- src/app/shared/components/product/product-configuration/product-configuration.component.ts
+++ src/app/shared/components/product/product-configuration/product-configuration.component.ts
@@ -1,20 +1,43 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  Validators,
+  FormsModule,
+} from '@angular/forms';
 import { SectionHeaderComponent } from '../../section-header/section-header.component';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { series } from '../../../dummy-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-configuration',
-  imports: [ReactiveFormsModule, SectionHeaderComponent],
+  imports: [
+    ReactiveFormsModule,
+    SectionHeaderComponent,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './product-configuration.component.html',
   styleUrl: './product-configuration.component.css',
 })
 export class ProductConfigurationComponent {
   private route = inject(ActivatedRoute);
+  private sanitizer = inject(DomSanitizer);
+
   id?: string;
+  series = series;
 
   heroTitle: String = '(nouveau produit)';
   isGeneralInfoOpen: boolean = false;
+
+  coverImageUrl: SafeUrl | string = '/image-placeholder.svg';
+  coverImageFile: File | null = null;
+  backImageUrl: SafeUrl | string = '/image-placeholder.svg';
+  backImageFile: File | null = null;
+
+  selectedSerieId: string | null = null;
 
   ngOnInit() {
     this.id = this.route.snapshot.queryParamMap.get('id') ?? undefined;
@@ -25,5 +48,17 @@ export class ProductConfigurationComponent {
     } else {
       // CREATE
     }
+  }
+
+  onFileChange(event: Event, isCover: boolean): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (!file) return;
+
+    isCover ? (this.coverImageFile = file) : (this.backImageFile = file);
+
+    const objectUrl = URL.createObjectURL(file);
+    isCover
+      ? (this.coverImageUrl = this.sanitizer.bypassSecurityTrustUrl(objectUrl))
+      : (this.backImageUrl = this.sanitizer.bypassSecurityTrustUrl(objectUrl));
   }
 }
